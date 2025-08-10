c6614f5cfb4117034a1789723ecae05656ecfba6
Mugilshark <94998254+Mugilshark@users.noreply.github.com>
2025-08-10 17:43:47 -0400

    Create PIDex.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDex.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class PIDex extends LinearOpMode {
+
+    double Kp = 0;
+    double Ki = 0;
+    double Kd = 0;
+    double integralSum = 0;
+
+    ElapsedTime timer = new ElapsedTime();
+    double lastError = 0;
+
+    public double PIDCONTROL(double reference, double state){
+        double error = reference - state;
+        integralSum += error * timer.seconds();
+        double derivative = (error - lastError) / timer.seconds();
+        lastError = error;
+
+        timer.reset();
+        double output = (error * Kp) + (derivative *Kd) + (integralSum *Ki);
+        return output;
+
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+
+        //Intializing Servos
+
+
+
+
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("left_front_drive");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("left_back_drive");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("right_front_drive");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("right_back_drive");
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            frontLeftMotor.setPower(PIDCONTROL(100, frontLeftMotor.getCurrentPosition()));
+
+
+        }
+    }
+}
